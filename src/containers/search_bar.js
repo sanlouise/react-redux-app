3d7d4805613e622b95f5229152bc569c078b5c0a
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-21 15:33:49 +0200

    Create controller search component

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { term: '' };
+		//To ensure 'this' is referring to the right scope on onInputChange (around line 14)
+		this.onInputChange = this.onInputChange.bind(this);
+	}
+
+	onInputChange(event) {
+		console.log(event.target.value);
+		this.setState({term: event.target.value});
+	}
+
 	render() {
 		return (
 			<form className="input-group">
-				<input />
+				<input 
+				placeholder="Retrieve the weather-forecast"
+				className="form-control"
+				value={this.state.term}
+				onChange={this.onInputChange}
+				/>
+				<span className="input-group-btn">
+					<button type="submit" className="btn btn-secondary">Search</button>
+				</span>
 			</form>
 		);
 	}
