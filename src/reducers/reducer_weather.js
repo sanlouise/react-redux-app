14332a4942322fb76ec1e06592b012c6fe21480c
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-24 11:43:38 +0200

    Fix typo in reducer_weather so that module can now be found

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -2,7 +2,7 @@ import { FETCH_WEATHER } from '../actions/index';
 
 export default function(state = [], action) {
 	switch (action.type) {
-		case FETCH_WEATHER;
+		case FETCH_WEATHER:
 		//An array because we have multiple cities to show and data as defined by API.. checked in console
 		//We can't use state.push, we can't directly change the data in state from the reducer
 		// Concat creates a new array with all old and new data in there.
